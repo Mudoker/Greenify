bb6e1f60b76241645b1ed0cac96e49c87b2a3316
Mudoker <huuquoc7603@gmail.com>
2023-12-10 15:40:00 +0700

    done

---
+++ app/src/main/java/com/example/greenify/Environment.java
@@ -0,0 +1,9 @@
+package com.example.greenify;
+
+public class Environment {
+    private static final String SUPPORT_EMAIL = "huuquoc7603@gmail.com";
+
+    public static String getSupportEmail() {
+        return SUPPORT_EMAIL;
+    }
+}
