6e2306572d667ae67b107cdf4c37e99e32ed592c
Mudoker <huuquoc7603@gmail.com>
2023-12-21 17:45:05 +0700

    Map Mini view

--- app/src/main/java/com/example/greenify/activity/map/OnPointAnnotationClickListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.greenify.activity.map
-
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
-
-interface OnPointAnnotationClickListener {
-    fun onPointAnnotationClick(annotation: PointAnnotation)
-}
