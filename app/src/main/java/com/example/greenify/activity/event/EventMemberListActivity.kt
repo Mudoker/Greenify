acced80e8c7c9615a838148b9e90dd9e7a06d4e8
Mudoker <huuquoc7603@gmail.com>
2023-12-22 17:30:53 +0700

    Member list view

---
+++ app/src/main/java/com/example/greenify/activity/event/EventMemberListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.greenify.activity.event
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.greenify.R
+
+class EventMemberListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_member_list)
+    }
+}
